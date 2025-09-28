585b8c7a7da3dc27b5e2f6f6a1ad42b2c844f661
kvvvotheOnnen <jsoto.ea@gmail.com>
2025-09-28 19:39:31 -0300

    Correccion en tiempo total de scrap de todos los modulos

--- Buscalibre/scrapEmAll.py
+++ Buscalibre/scrapEmAll.py
@@ -23,11 +23,11 @@ def main():
         csv_relative_path = f"data/{csv_filename}"
         start_time = datetime.now()
         process_logs(f"\n🚀 Iniciando ciclo de scraping - {start_time.strftime('%Y-%m-%d %H:%M:%S')}")
-        end_time = datetime.now()
         try:
             scrapear_aleatoriamente(categorias, 4 ,2)
         except ValueError as err:
             error_logs('en buscaLibre/scrapEmAll.py, flujo principal: ',err)
+        end_time = datetime.now()
         elapsed_time = end_time - start_time
         process_logs(f"⏳Tiempo total del scrap: {elapsed_time}")
         wait_hours = random.uniform(1, 4)
--- feriaChilena/scrapemAll.py
+++ feriaChilena/scrapemAll.py
@@ -49,11 +49,11 @@ def main():
         csv_relative_path = f"data/{csv_filename}"
         start_time = datetime.now()
         process_logs(f"\n🚀 Iniciando ciclo de scraping - {start_time.strftime('%Y-%m-%d %H:%M:%S')}")
-        end_time = datetime.now()
         try:
             scrapear_aleatoriamente(categorias, 3,1)
         except ValueError as err:
             error_logs('en feriaChilena/scrapEmAll.py, flujo principal: ',err)
+        end_time = datetime.now()
         elapsed_time = end_time - start_time
         process_logs(f"⏳Tiempo total del scrap: {elapsed_time}")
         wait_hours = random.uniform(1, 4)
